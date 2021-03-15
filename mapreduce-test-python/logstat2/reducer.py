d60f37cb51194b03626c3bac9cb9ebb1bd70c10e
yitingwang56 <80591602+yitingwang56@users.noreply.github.com>
2021-03-14 23:30:49 -0400

    Update reducer.py

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -1,24 +1,34 @@
+#!/usr/bin/python
 import sys
 from operator import itemgetter
 from collections import defaultdict
 
-
-top = defaultdict(list)
+dict_ip_count = {}
 
 for line in sys.stdin:
-    line = line.strip().split('\t')
-    hr_ip, count = line
-    hr,ip = hr_ip.split(' ')
+    line = line.strip()
+    ip, num = line.split('\t')
     try:
-        hr = int(hr)
-        count = int(count)
-        top[hr].append([ip,count])
+        num = int(num)
+        dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
 
     except ValueError:
         pass
 
+sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(1), reverse=True)
+top = defaultdict(list)
+
+for hr_ip,count in sorted_dict_ip_count:
+    hr = hr_ip[1:3]
+    hr=int(hr)
+    ip=hr_ip[7:]
+    count=int(count)
+    top[hr].append([ip,count])
+    
 
 
- for i in range(24):
-    t3 = sorted(top[i],key=lambda x:x[1], reverse=True)[:3]
-    print '%s\t%s' % (i,t3)
+
+
+for i in range(24):
+    top_3 = sorted(top[i],key=lambda x:x[1],reverse=True)[:3]
+    print '%s\t%s' % (i,top_3)
