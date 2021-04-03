e0d6d444a1aa0dded046d031c6081f0fcbb80c56
wangyepu888 <34576117+wangyepu888@users.noreply.github.com>
2021-04-03 20:53:20 +0800

    Update test.sh

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /count/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /count/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /count/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/shot_logs.csv /count/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/shot_logs.csv/count/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../../mapreduce-test-python/count/mapper.py -mapper ../../mapreduce-test-python/count/mapper.py \
 -file ../../mapreduce-test-python/count/reducer.py -reducer ../../mapreduce-test-python/count/reducer.py \
